92bb02f4c9a24a09ee8e24fae8f4bbf1f62e1018
DarekDGB <majescyk@gmail.com>
2025-12-30 00:19:12 +0000

    test(core): fix bridge test to use DerivationPath.parse

--- tests/test_bridge_hdnode_to_address.py
+++ tests/test_bridge_hdnode_to_address.py
@@ -6,7 +6,7 @@ from core.wallet.bridge import BridgeError, address_from_node
 
 def test_bridge_address_from_node_starts_with_D():
     seed = bytes.fromhex("000102030405060708090a0b0c0d0e0f")
-    path = DerivationPath.from_string("m/44'/20'/0'/0/0")
+    path = DerivationPath.parse("m/44'/20'/0'/0/0")
 
     node = path.derive_from_seed(seed)
     addr = address_from_node(node)
